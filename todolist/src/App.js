145d041f84b6b9210ca69f6d882938b3579cebd0
Aman <amanyadav6750@gmail.com>
2023-03-30 15:12:20 +0530

    use state in react

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,12 +4,43 @@ import Header from './MyComponents/Header';
 import { Footer } from './MyComponents/Footer';
 import {TodoItem} from './MyComponents/TodoItem';
 import { Todos } from './MyComponents/Todos';
+import React, { useState } from 'react';
 
 function App() {
+
+    const onDelete = (todo)=>{
+        console.log("i am on delete", todo );
+        // in react we can delete like this using inndex , in other javascript and angular like we can , in react we can use [use state] to delete 
+        //  let index = todos.indexOf(todo);
+        //  todos.splice(index,1);
+
+        
+        
+    }
+
+  let todos = [
+    {
+      sno:1,
+      title: "Go to the market",
+      desc: "you need to go to the market to get this job done "
+    },
+    {
+      sno:2,
+      title: "Go to the mall",
+      desc: "you need to go to the mall to get this job done "
+    },
+    {
+      sno:3,
+      title: "Go to the charbagh",
+      desc: "you need to go to the charbagh to get this job done "
+    },
+
+  ]
+
   return (
     <>
     <Header title="My Todos List" searchBar={true}/>
-    <Todos/>
+    <Todos todos={todos} onDelete={onDelete}/>
     <Footer/>
     </>
   );
--- todolist/src/MyComponents/Todos.js
+++ todolist/src/MyComponents/Todos.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import {TodoItem} from "./TodoItem";
 
-export const Todos = () => {
+export const Todos = (props) => {
     return (
 
-        <div>
+        <div className="container">
 
-            Todos works 
+            <h3 className="text-center my-3">Todos List</h3>
+            {props.todos.map((todo)=>{
+                return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+            })}
+
         </div>
     )
 }
