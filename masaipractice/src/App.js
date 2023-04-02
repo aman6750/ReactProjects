a8399dab448c8268587b50a13a6d9ab94c34a791
Aman <amanyadav6750@gmail.com>
2023-04-02 23:36:18 +0530

    styling in react

--- masaipractice/src/App.js
+++ masaipractice/src/App.js
@@ -23,31 +23,49 @@ let data = [
 
 
 const Item = ({data}) => {
+
   <li>
     <div>{data.name}</div>
     <div>{data.location}</div>
   </li>
+
+}
+
+let style1={
+  fontSize:'30px',
+  background:'gray'
+}
+
+let style2={
+  borderRadius:20,
+  background:'teal'
 }
 
 
 function App() {
   return (
    <>
-    <div>
-      <button> ONE </button>
+
+    <div className="one">
+      <button style={style1}> ONE </button>
     </div>
-    <div>
-    <button> TWO </button>
+
+    <div className="two">
+    <button style={{fontSize:'100px',background:'cyan'}}> TWO </button>
     </div>
-  <div>
-  <button> THREE </button>
-</div>
-<div>
+
+    <div className="three">
+      <button style={{...style1,...style2}}> THREE </button>
+    </div>
+
+    <div className="four">
       <button> FOUR </button>
     </div>
-    <div>
-    <button> FIVE </button>
-  </div>
+
+    <div className="five">
+    <button className='class1 class2'> FIVE </button>
+    </div>
+
    </>
   );
 }
